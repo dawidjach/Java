70893a5331b609081a336b99bf2adfdbba2add18
dejvhacker <hamavanti1@gmail.com>
2023-05-30 22:18:21 +0200

    blackjack update

---
+++ besucherVerwaltung/ComeLeave.java
@@ -0,0 +1,12 @@
+package besucherVerwaltung;
+
+
+import java.util.Date;
+
+public class ComeLeave {
+    Date date = new Date();
+    int comeInOut = 0;
+    int noPeople = 0;
+    int[][] countVisitors = new int[9][30];
+
+}
